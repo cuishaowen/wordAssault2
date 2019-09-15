dbe122cf122cbb029123bc462a003bf8dcff07ce
cuishaowen <cuishaowen2013@qq.com>
2019-09-15 19:17:53 +0800

    修改

--- src/main/webapp/webclient/js/bgmusic.js
+++ src/main/webapp/webclient/js/bgmusic.js
@@ -1,6 +1,3 @@
-function alreadyBuyCourses(courseId) {
-    window.location.href = getContextPath() + '/webclient/dcxl.html?courseId=' + courseId;
-}
 setTimeout(function(){
     var x = document.getElementById("music");
     x.src='videos/bgmusic.mp3';
